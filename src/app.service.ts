2a889ebb8c2af2db973e796b1b64a39f39e2b588
sinawic <sinawic@hasebgostar.com>
2021-12-21 16:08:45 +0330

    first commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+
+const baseURL = 'https://api.blockcypher.com/v1/eth/main/txs/',
+  transactionId = '0x552865a0dbbc5169a1da606598d3908f2652bd656e021bf74acd5f3b2d93591d'
+
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getTransactionDetail() {
+    try {
+      const response = await axios.get(baseURL + transactionId);
+      return ('<h1>the transaction detail</h1> <pre>' +
+        JSON.stringify(response.data).toString().replace(/,/g, ', <br/>')
+          .replace(`"total"`, `<b>"total"</b>`)
+          .replace(`"confirmed"`, `<b>"confirmed"</b>`)
+          .replace(`"confirmations"`, `<b>"confirmations"</b>`)
+        + '</pre>');
+    } catch (error) {
+      console.error(error);
+    }
+    return ('something went wrong')
+  }
+
+  async getHello() {
+    return await this.getTransactionDetail();
   }
 }
